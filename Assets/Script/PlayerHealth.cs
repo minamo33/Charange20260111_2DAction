3fed653b24459348cdf2019df739209f12a9ae22
もんたろう <minamo0329@gmail.com>
2026-01-13 03:28:20 +0900

    MainMapにUI、プレイヤー移動量、フレーム固定追加 TMP追加したので少し重いﾖ

--- Assets/Script/PlayerHealth.cs
+++ Assets/Script/PlayerHealth.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int currentHealth;
     public int maxHealth;
 
+    //UI’Ç‰Á
+    public TMP_Text healthText;
+    public Animator healthTextAnim;
+
+    private void Start()
+    {
+        healthText.text = "HP:" + currentHealth + " / " + maxHealth;
+    }
     public void ChangeHealth(int amount)
     {
         currentHealth += amount;
-        if(currentHealth <= 0)
+        healthTextAnim.Play("TextUpdate");
+        healthText.text = "HP:" + currentHealth + " / " + maxHealth;
+
+        if (currentHealth <= 0)
         {
             gameObject.SetActive(false);
         }
